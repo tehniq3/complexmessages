92a426c1c07891051add32b6fc62230f03b40f60
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2019-05-30 22:33:38 +0300

    Update tx_1.ino

--- tx_1/tx_1.ino
+++ tx_1/tx_1.ino
@@ -1,18 +1,9 @@
-/*
- * SerialReceiveMultipleFields sketch - https://www.oreilly.com/library/view/arduino-cookbook/9781449399368/ch04.html
- * This code expects a message in the format: 12,345,678
- * This code requires a newline character to indicate the end of the data
- * Set the serial monitor to send newline characters
- * receiver v.1
- * sketch writed by Nicu FLORICA - http://www.arduinotehniq.com
- */
+// CommaDelimitedOutput sketch - https://www.oreilly.com/library/view/arduino-cookbook/9781449399368/ch04.html
+// sender v.1 using SoftwareSerial 
+// sketch writed by Nicu FLORICA - http://www.arduinotehniq.com
 
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3); // RX, TX
-
-const int NUMBER_OF_FIELDS = 3; // how many comma separated fields we expect
-int fieldIndex = 0;            // the current field being received
-int values[NUMBER_OF_FIELDS];   // array holding values for all the fields
 
 void setup()
 {
@@ -22,31 +13,32 @@ void setup()
 
 void loop()
 {
-  if( mySerial.available())
-  {
-    char ch = mySerial.read();
-    if(ch >= '0' && ch <= '9') // is this an ascii digit between 0 and 9?
-    {
-      // yes, accumulate the value
-      values[fieldIndex] = (values[fieldIndex] * 10) + (ch - '0'); 
-    }
-    else if (ch == ',')  // comma is our separator, so move on to the next field
-    {
-      if(fieldIndex < NUMBER_OF_FIELDS-1)
-        fieldIndex++;   // increment field index
-    }
-    else
-    {
-      // any character not a digit or comma ends the acquisition of fields
-      // in this example it's the newline character sent by the Serial Monitor
-      Serial.print( fieldIndex +1);
-      Serial.println(" fields received:");
-      for(int i=0; i <= fieldIndex; i++)
-      {
-        Serial.println(values[i]);
-        values[i] = 0; // set the values to zero, ready for the next message
-      }
-      fieldIndex = 0;  // ready to start over
-    }
-  }
+  int value1 = 10;    // some hardcoded values to send
+  int value2 = 100;
+  int value3 = 1000;
+
+//  Serial.print('H'); // unique header to identify start of message
+//  Serial.print(",");
+  Serial.print(value1,DEC);
+  Serial.print(",");
+  Serial.print(value2,DEC);
+  Serial.print(",");
+  Serial.print(value3,DEC);
+  Serial.print(",");  // note that a comma is sent after the last field
+  Serial.println();  // send a cr/lf
+  delay(100);
+
+//  mySerial.print('H'); // unique header to identify start of message
+//  mySerial.print(",");
+  mySerial.print(value1,DEC);
+  mySerial.print(",");
+  mySerial.print(value2,DEC);
+  mySerial.print(",");
+  mySerial.print(value3,DEC);
+  mySerial.print(",");  // note that a comma is sent after the last field
+  mySerial.print(" ");  // send a cr/lf
+  delay(100);
+
+
+  delay (2000);
 }
